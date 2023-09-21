e5de1180c3ca6d3497086a5bf90a1cc9cc54fa2f
quyhuynh49 <quyhuynh49@gmail.com>
2023-09-21 17:11:44 +0700

    create debounce fn

--- script.js
+++ script.js
@@ -1,34 +1,41 @@
 let timeout = null;
 
-document.getElementById("search_field").addEventListener("input", (event) => {
-
-  clearTimeout(timeout);
-
-  timeout = setTimeout(() => {
-    fetch(
-      "https://dummyjson.com/users/search?" +
-        new URLSearchParams({
-          q: event.target.value,
-        })
-    )
-      .then((resp) => resp.json())
-      .then((resp) => {
-        document.getElementById("result_box").innerHTML = "";
-        if (resp?.users?.length) {
-          resp.users.forEach((user) => {
-            const textElement = document.createElement("p");
-            const userName = document.createTextNode(
-              `${user.firstName} ${user.lastName}`
-            );
-            textElement.appendChild(userName);
-
-            document.getElementById("result_box").appendChild(textElement);
-          });
-        }
+const debounce = (callback, deplay) => {
+  let timeout = null;
+  return (...args) => {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      callback(...args); // spread operator
+    }, deplay);
+  };
+};
+const handleInputChange = (event) => {
+  fetch(
+    "https://dummyjson.com/users/search?" +
+      new URLSearchParams({
+        q: event.target.value,
       })
-      .catch((err) => {
-        console.error("ERROR", err);
-      });
-  }, 1000);
-  
-});
+  )
+    .then((resp) => resp.json())
+    .then((resp) => {
+      document.getElementById("result_box").innerHTML = "";
+      if (resp?.users?.length) {
+        resp.users.forEach((user) => {
+          const textElement = document.createElement("p");
+          const userName = document.createTextNode(
+            `${user.firstName} ${user.lastName}`
+          );
+          textElement.appendChild(userName);
+
+          document.getElementById("result_box").appendChild(textElement);
+        });
+      }
+    })
+    .catch((err) => {
+      console.error("ERROR", err);
+    });
+};
+
+document
+  .getElementById("search_field")
+  .addEventListener("input", debounce(handleInputChange, 1000));
